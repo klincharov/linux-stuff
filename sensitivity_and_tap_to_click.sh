ba1f723b0ba2bd90d1bd01fc620de3fc18a613ee
Georgi Klincharov <klincharov@gmail.com>
2015-03-11 08:23:14 +0100

    Create sensitivity_and_tap_to_click.sh

---
+++ sensitivity_and_tap_to_click.sh
@@ -0,0 +1,3 @@
+sudo echo -n 120 > /sys/devices/platform/i8042/serio1/serio2/speed
+sudo echo -n 200 > /sys/devices/platform/i8042/serio1/serio2/sensitivity
+sudo echo -n 1 > /sys/devices/platform/i8042/serio1/serio2/press_to_select
